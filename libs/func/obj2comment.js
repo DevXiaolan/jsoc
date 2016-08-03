110775366af8de9a34ff2da15aeca314ef2fd7d1
captainblue2013 <admin@lanhao.name>
2016-08-03 14:26:45 +0800

    object to comment

--- libs/func/obj2comment.js
+++ libs/func/obj2comment.js
@@ -3,9 +3,50 @@
  */
 
 'use strict';
+const fs = require('fs');
+const os = require('os');
+const EOL = (os && os.EOL)?os.EOL:'\n';
 
-let obj2comment = function (obj, indent) {
+let obj2comment = function (obj) {
+  let output = '/** '+EOL+' * @jsoc '+EOL;
+  output += rParse(obj, 1);
+  return output +' */';
+};
 
+let rParse = function (obj, indent) {
+  let output = '';
+  for(let k in obj){
+    output += ' * '+' '.repeat(indent*2);
+    output += k;
+    if(typeof obj[k] !== 'object'){
+      output += ':'+obj[k]+EOL;
+    }else{
+      output += EOL+rParse(obj[k],indent+1);
+    }
+
+  }
+  return output;
 };
 
 module.exports = obj2comment;
+
+console.log(obj2comment({
+  name: 'test',
+  request: {
+    query: {
+      appId: {
+        _type:'number'
+      }
+    }
+  },
+  response: {
+    body: {
+      code :{
+        _assert:200
+      },
+      data: {
+        _type:'object'
+      }
+    }
+  }
+}));
