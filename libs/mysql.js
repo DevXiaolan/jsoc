11a02dd94a734a049337b18e472eb5c242ebc7b4
captainblue2013 <admin@lanhao.name>
2017-04-27 14:13:16 +0800

    shit

--- libs/mysql.js
+++ libs/mysql.js
@@ -24,36 +24,6 @@ mysql.link  = function(config){
 
 
 mysql.config = {
-    'rds':{
-        'host':'rds0y91rwy6n8d3kmu70.mysql.rds.aliyuncs.com',
-        'user':'ciweidb',
-        'password':'ciweidb2015',
-        'database':'message'
-    },
-    'rds_remote':{
-        'host':'rds0y91rwy6n8d3kmu70public.mysql.rds.aliyuncs.com',
-        'user':'ciweidb',
-        'password':'ciweidb2015',
-        'database':'message'
-    },
-    'rds_r':{
-        'host':'rds0yqady2a50voqc3fx.mysql.rds.aliyuncs.com',
-        'user':'ciweidb',
-        'password':'ciweidb2015',
-        'database':'message'
-    },
-    'rds_r_remote':{
-        'host':'rds0yqady2a50voqc3fxpublic.mysql.rds.aliyuncs.com',
-        'user':'ciweidb',
-        'password':'ciweidb2015',
-        'database':'message'
-    },
-    'dev':{
-        'host':'121.41.85.236',
-        'user':'ciwei',
-        'password':'123456',
-        'database':'message'
-    },
     'local':{
         'host':'127.0.0.1',
         'user':'ciwei',
